923377ab8c99bdccbb0465af2db2c163b20b4e29
Harry Tom <61614900+Harry260@users.noreply.github.com>
2020-12-18 08:36:04 +0530

    Update story.js

--- js/story.js
+++ js/story.js
@@ -13,7 +13,8 @@ function get_data_async(){
                 .replace(/:b/g, "<span class='bold'>").replace(/b:/g, "</span>")
                 .replace(/:h/g, "<br><br><span class='heading'>").replace(/h:/g, "</span><br>")
                 .replace(/:i/g, "<span class='italic'>").replace(/i:/g, "</span>")
-                .replace(/:c/g, "<span class='code'>").replace(/c:/g, "</span>");
+                .replace(/:c/g, "<span class='code'>").replace(/c:/g, "</span>")
+                .replace(/\n/g, "<br>")
 
                 $( '#view-title' ).text(snapshot.val().title);
                 $( '#view-description' ).html(content);
