98119227418e9fd3e108ffb6f79f273c9b780fff
mement-stories <76152512+mement-stories@users.noreply.github.com>
2021-01-26 12:48:47 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,17 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyD1x5yuPxL-owYUrdG7bx61OcvmV2Kb_bE",
+ apiKey: "AIzaSyD1x5yuPxL-owYUrdG7bx61OcvmV2Kb_bE",
     authDomain: "mement-stories.firebaseapp.com",
-    projectId: "mement-stories",
+   projectId: "mement-stories",
     storageBucket: "mement-stories.appspot.com",
     messagingSenderId: "190886731253",
-    appId: "1:190886731253:web:ce04f029ea324afb5814bc"
+    appId: "1:190886731253:web:d822207c874f56515
   };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
   
-firebase.auth.Auth.Persistence.LOCAL;
+firebase.auth.Auth.Persistence.LOCAL; 
+
+
+
+
