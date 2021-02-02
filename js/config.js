dc23eb6efad8c1589a0764417a09d92e2fbc57ca
Harry Tom <Harry260@users.noreply.github.com>
2021-02-02 11:06:43 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ var firebaseConfig = {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 apiKey: "AIzaSyD1x5yuPxL-owYUrdG7bx61OcvmV2Kb_bE",
   authDomain: "mement-stories.firebaseapp.com",
  databaseURL: "https://mement-stories-default-rtdb.firebaseio.com",
@@ -9,6 +10,14 @@ apiKey: "AIzaSyD1x5yuPxL-owYUrdG7bx61OcvmV2Kb_bE",
     storageBucket: "mement-stories.appspot.com",
     messagingSenderId: "190886731253",
     appId: "1:190886731253:web:d822207c874f56515814bc"
+=======
+    apiKey: "AIzaSyD1x5yuPxL-owYUrdG7bx61OcvmV2Kb_bE",
+    authDomain: "mement-stories.firebaseapp.com",
+    projectId: "mement-stories",
+    storageBucket: "mement-stories.appspot.com",
+    messagingSenderId: "190886731253",
+    appId: "1:190886731253:web:ce04f029ea324afb5814bc"
+>>>>>>> parent of 9811922... Update config.js
 =======
     apiKey: "AIzaSyD1x5yuPxL-owYUrdG7bx61OcvmV2Kb_bE",
     authDomain: "mement-stories.firebaseapp.com",
